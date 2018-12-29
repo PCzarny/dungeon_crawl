b944fd46d92349705fcf6c2a2247fe4122eb9e21
Piotr Czarny <piotr.a.czarny@gmail.com>
2018-12-29 15:18:41 +0100

    Replace display protocol with String.Chars

--- lib/dungeon_crawl/room/action.ex
+++ lib/dungeon_crawl/room/action.ex
@@ -4,5 +4,9 @@ defmodule DungeonCrawl.Room.Action do
 
   def forward, do: %Action{id: :forward, label: "Move forward."}
   def rest, do: %Action{id: :rest, label: "Take a better look and rest."}
-  def seearch, do: %Action{id: :search, label: "Search the room."}
+  def search, do: %Action{id: :search, label: "Search the room."}
+
+  defimpl String.Chars do
+    def to_string(action), do: action.label
+  end
 end
